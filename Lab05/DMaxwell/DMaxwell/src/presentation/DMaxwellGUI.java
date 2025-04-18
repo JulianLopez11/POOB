bf806f341b30a50e2c9fd50a51c5d9d3189800e3
JulianLopez11 <julianl116910@gmail.com>
2025-04-17 21:20:32 -0500

    Prueba Main

--- Lab05/DMaxwell/DMaxwell/src/presentation/DMaxwellGUI.java
+++ Lab05/DMaxwell/DMaxwell/src/presentation/DMaxwellGUI.java
@@ -148,5 +148,6 @@ public class DMaxwellGUI extends JFrame {
     public static void main(String[] args) {
         DMaxwellGUI gui = new DMaxwellGUI();
         gui.setVisible(true);
+        System.out.println("Hola");
     }
 }
