8a5547faf4b134d1a4770d300439263e0aa83575
Juan Puentes <seb123.juli@gmail.com>
2025-04-26 18:04:55 -0500

    ELEE

--- Lab05/DMaxwell/src/domain/DMaxwellExceptions.java
+++
@@ -1,8 +0,0 @@
-package domain;
-public class DMaxwellExceptions extends Exception{
-    public static final String ONLY_POSITIVE_DIMENTIONS = "Dimensiones No Enteras";
-    public DMaxwellExceptions(String message){
-        super(message);
-    }
-}
-
